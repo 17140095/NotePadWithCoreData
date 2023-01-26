bc849ad4fa6f0b943a6b0af5e051326570440fb0
Ali Raza <17140095@gift.edu.pk>
2023-01-26 18:18:47 +0500

    Initial Commit

---
+++ NotePadWithCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotePadWithCoreData
+//
+//  Created by Ali Raza on 26/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
